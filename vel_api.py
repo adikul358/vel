c178088421c0576d39acb8a11d967a193f0452d5
Aditya Kulshrestha <adi.kul358@gmail.com>
2021-07-27 08:23:09 +0530

    waste

--- vel_api.py
+++ vel_api.py
@@ -7,9 +7,6 @@ from pymongo import MongoClient
 import httplib2
 from oauth2client import client
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from uuid import uuid1
 import json
 import os
