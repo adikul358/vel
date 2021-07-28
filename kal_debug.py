b01aa1427d42edea1d714b7185c3e26b08271d41
Aditya Kulshrestha <adi.kul358@gmail.com>
2021-07-29 03:17:12 +0530

    server edits

--- kal_debug.py
+++ kal_debug.py
@@ -13,32 +13,46 @@ users_col = db['users']
 
 user_data = users_col.find_one({'email': 'kalyaani.manoj@gmail.com'})
 
+
 json_token = user_data['json_token']
 email = user_data['email']
 section = user_data['section']
 subjects = user_data['subjects']
 
-creds = client.AccessTokenCredentials.from_json(json_token)
+creds = client.GoogleCredentials.from_json(json_token)
 http = creds.authorize(httplib2.Http())
-service = build('calendar', 'v3', http=http)
+try: 
+    service = build('calendar', 'v3', credentials=creds)
+except client.AccessTokenCredentialsError:
+    print("Access token expired")
+    creds.refresh(http)
+    if creds.to_json() != print("Access token refreshed")
+    user_col.update_one(
+        {'email': 'kalyaani.manoj@gmail.com'},
+        {'$set': {'json_token': creds.to_json()}}
+    )
+    print("Credentials updated in database")
+    service = build('calendar', 'v3', credentials=creds)
 
 page_token = None
-vel_calender_id = ""
+vel_calendar_id = ""
 while True:
   calendar_list = service.calendarList().list(pageToken=page_token).execute()
   for calendar_list_entry in calendar_list['items']:
     calendar_summary = calendar_list_entry['summary']
-    print(calendar_summary)
+    # print(calendar_summary)
     if calendar_summary == "VEL":
-      vel_calender_id = calendar_list_entry['id']
-      print(f'vel_calender_id: {vel_calender_id}')
+      vel_calendar_id = calendar_list_entry['id']
+      print(f'vel_calendar_id: {vel_calendar_id}')
       break
   page_token = calendar_list.get('nextPageToken')
   if not page_token: break
 
+if not vel_calendar_id: raise RuntimeError("VEL calendar not present")
+
 page_token = None
 while True:
-  events = service.events().list(calendarId=vel_calender_id, pageToken=page_token).execute()
+  events = service.events().list(calendarId=vel_calendar_id, pageToken=page_token).execute()
   for event in events['items']:
     print(event['summary'])
   page_token = events.get('nextPageToken')
